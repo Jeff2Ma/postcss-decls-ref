a0bf8208da315570569cb2b210742ac27303b7b0
Jeff2Ma <mjh2316@gmail.com>
2017-04-01 20:58:03 +0800

    Add more test.

--- test/test.js
+++ test/test.js
@@ -39,6 +39,65 @@ describe('postcss-decls-ref Unit Test', function () {
 				.on('data', noop)
 				.on('end', done);
 		});
+
+		it('Support for @media context.', function (done) {
+			vfs.src('./test/css/t3.css')
+				.pipe(postcss([ref()]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t3.1.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t3.1.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+		it('Mulit target selectors are work well.', function (done) {
+			vfs.src('./test/css/t4.css')
+				.pipe(postcss([ref()]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t4.1.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t4.1.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+		it('Work well when target selector is part of group selectors.', function (done) {
+			vfs.src('./test/css/t5.css')
+				.pipe(postcss([ref()]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t5.1.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t5.1.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+
+		it('Sub Class contexts are work well.', function (done) {
+			vfs.src('./test/css/t6.css')
+				.pipe(postcss([ref()]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// // fs.writeFileSync('./test/css/t6.1.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t6.1.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+
 	});
 
 	describe('Option `refMod` is `group`:', function () {
@@ -57,6 +116,7 @@ describe('postcss-decls-ref Unit Test', function () {
 				.on('end', done);
 		});
 
+
 		it('Different Orders are not effect.', function (done) {
 			vfs.src('./test/css/t2.css')
 				.pipe(postcss([ref({refMod: 'group'})]))
@@ -64,6 +124,63 @@ describe('postcss-decls-ref Unit Test', function () {
 					var content = file.contents.toString();
 					// fs.writeFileSync('./test/css/t2.2.css', content, 'utf8');
 					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t2.2.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+		it('Support for @media context.', function (done) {
+			vfs.src('./test/css/t3.css')
+				.pipe(postcss([ref({refMod: 'group'})]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t3.2.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t3.2.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+		it('Mulit target selectors are work well.', function (done) {
+			vfs.src('./test/css/t4.css')
+				.pipe(postcss([ref({refMod: 'group'})]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t4.2.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t4.2.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+		it('Work well when target selector is part of group selectors.', function (done) {
+			vfs.src('./test/css/t5.css')
+				.pipe(postcss([ref({refMod: 'group'})]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t5.2.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t5.2.css'), {encoding: 'utf8'});
+					cssExpected.should.be.equal(content);
+					cb();
+				}))
+				.on('data', noop)
+				.on('end', done);
+		});
+
+
+		it('Sub Class contexts are work well.', function (done) {
+			vfs.src('./test/css/t6.css')
+				.pipe(postcss([ref({refMod: 'group'})]))
+				.pipe(through2.obj(function (file, enc, cb) {
+					var content = file.contents.toString();
+					// fs.writeFileSync('./test/css/t6.2.css', content, 'utf8');
+					var cssExpected = fs.readFileSync(path.resolve(process.cwd(), './test/css/t6.2.css'), {encoding: 'utf8'});
 					cssExpected.should.be.equal(content);
 					cb();
 				}))
